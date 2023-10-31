1543df0e22204ecb1e23b1a294e803a5e27614af
objektwerks <objektwerks@runbox.com>
2023-10-31 13:46:36 -0400

    enhanced server

--- src/main/scala/objektwerks/Server.scala
+++ src/main/scala/objektwerks/Server.scala
@@ -3,9 +3,17 @@ package objektwerks
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
+import io.helidon.webserver.WebServer
+
 object Server extends LazyLogging:
   private val config = ConfigFactory.load("server.conf")
   private val host = config.getString("host")
   private val port = config.getInt("port")
 
-  logger.info(s"todo: server {$host:$port}")
+  val server = WebServer
+    .builder()
+    .host(host)
+    .port(port)
+    .build()
+
+  logger.info(s"Server running @ {$host:$port}")
