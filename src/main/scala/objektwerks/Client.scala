29bc0c1ddd485966f6fd758f8cf47e2fc933fb87
objektwerks <objektwerks@runbox.com>
2023-10-31 14:14:50 -0400

    enhanced client

--- src/main/scala/objektwerks/Client.scala
+++ src/main/scala/objektwerks/Client.scala
@@ -2,5 +2,14 @@ package objektwerks
 
 import com.typesafe.scalalogging.LazyLogging
 
+import io.helidon.webclient.api.WebClient
+
 object Client extends LazyLogging:
-  logger.info("todo: client")
+  val url = "http://localhost:7979/now"
+
+  val client = WebClient
+    .builder()
+    .baseUri(url)
+    .build()
+
+  logger.info(s"*** Client targeting: $url")
