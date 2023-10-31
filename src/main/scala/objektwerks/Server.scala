f99f78e882fc0a2a656e8085a2d2f67d52a0fb0a
objektwerks <objektwerks@runbox.com>
2023-10-31 14:03:57 -0400

    refactored server

--- src/main/scala/objektwerks/Server.scala
+++ src/main/scala/objektwerks/Server.scala
@@ -1,13 +1,24 @@
 package objektwerks
 
-import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
 import io.helidon.config.Config
 import io.helidon.webserver.WebServer
+import io.helidon.webserver.http.HttpRouting
+
+import java.time.Instant
 
 object Server extends LazyLogging:
   val config = Config.create()
-  val server = WebServer.create(routing, config.get("server"))
 
-  logger.info(s"Server running @ ${server.host}:${server.port}")
+  val routing = HttpRouting
+    .builder()
+    .post("/now", (request, response) => { response.send(Instant.now.toString) } )
+
+  val server = WebServer
+    .builder()
+    .config(config.get("server"))
+    .routing(routing)
+    .build()
+
+  logger.info(s"Server running @ 127.0.0.1:${server.port}")
