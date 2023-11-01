ac6da6cd198ce00127d92b581570a8707ac6bf3a
objektwerks <objektwerks@runbox.com>
2023-11-01 06:39:34 -0400

    formatted server

--- src/main/scala/objektwerks/Server.scala
+++ src/main/scala/objektwerks/Server.scala
@@ -12,29 +12,29 @@ import java.time.Instant
   * WebServer appears to set port incorrectly, always returning -1.
   */
 object Server extends LazyLogging:
-  @main def runServer(): Unit =
-    val config = Config.create()
-    val host = config.get("server.host").asString().get()
-    val port = config.get("server.port").asInt().get()
+  @main def runServer: Unit =
+    val config = Config.create
+    val host = config.get("server.host").asString.get
+    val port = config.get("server.port").asInt.get
     logger.info(s"*** Config host: $host port: $port")
 
     val routing = HttpRouting
-      .builder()
+      .builder
       .get("/now", (request, response) => response.send(Instant.now.toString))
 
     val server = WebServer
-      .builder()
+      .builder
       .host(host)
       .port(port)
       .routing(routing)
-      .build()
+      .build
 
-    logger.info(s"*** Helidon WebServer port: ${server.port()}")
+    logger.info(s"*** Helidon WebServer port: ${server.port}")
     logger.info(s"*** Server running @ $host:$port")
 
     sys.addShutdownHook {
       logger.info(s"*** Server stopping @ $host:$port")
-      server.stop()
+      server.stop
     }
 
-    Thread.currentThread().join()
+    Thread.currentThread.join
