be428125702fe146447fe023f626f2fc78e90b3b
objektwerks <objektwerks@runbox.com>
2023-11-11 10:52:58 -0500

    refactored web client

--- src/main/scala/objektwerks/Client.scala
+++ src/main/scala/objektwerks/Client.scala
@@ -6,20 +6,18 @@ import io.helidon.webclient.api.WebClient
 
 object Client extends LazyLogging:
   @main def runClient: Unit =
-    val baseUrl = "http://localhost:7979"
-    val endpoint = "/now"
+    val baseUrl = "http://localhost:7979/now"
 
     val client = WebClient
       .builder
       .baseUri(baseUrl)
       .build
     
-    logger.info(s"*** Client targeting: $baseUrl$endpoint")
+    logger.info(s"*** Client targeting: $baseUrl")
 
     val response = client
       .get
-      .path(endpoint)
       .request
-    val now = response.entity
+    val now = response.entity.as(classOf[String])
 
     logger.info(s"*** Server response: ${now.toString}")
