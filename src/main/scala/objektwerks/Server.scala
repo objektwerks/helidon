180b60b767272432142e3264c2bec18e5a295a08
objektwerks <objektwerks@runbox.com>
2023-11-11 16:31:38 -0500

    added handler to server

--- src/main/scala/objektwerks/Server.scala
+++ src/main/scala/objektwerks/Server.scala
@@ -2,21 +2,29 @@ package objektwerks
 
 import io.helidon.config.Config
 import io.helidon.webserver.WebServer
-import io.helidon.webserver.http.HttpRouting
+import io.helidon.webserver.http.{Handler, HttpRouting}
 
 import java.time.Instant
 
 import scala.sys.process.Process
+import io.helidon.webserver.http.ServerRequest
+import io.helidon.webserver.http.ServerResponse
 
+final class NowHandler extends Handler:
+  override def handle(request: ServerRequest,
+                      response: ServerResponse): Unit = response.send(s"*** Datetime: ${Instant.now.toString}")
+  
 object Server:
   @main def runServer: Unit =
     val config = Config.create.get("server")
     val url = config.get("url").asString.get
     val endpoint = config.get("endpoint").asString.get
 
+    val handler = NowHandler()
+
     val routing = HttpRouting
       .builder
-      .get(endpoint, (request, response) => response.send(s"*** Datetime: ${Instant.now.toString}"))
+      .get(endpoint, handler)
 
     WebServer
       .builder
