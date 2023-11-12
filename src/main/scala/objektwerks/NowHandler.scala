d995e161c7e3e7dcaf85d937c207ade1c0ab317d
objektwerks <objektwerks@runbox.com>
2023-11-12 06:45:49 -0500

    reformatted now handler

--- src/main/scala/objektwerks/NowHandler.scala
+++ src/main/scala/objektwerks/NowHandler.scala
@@ -6,4 +6,5 @@ import java.time.Instant
 
 final class NowHandler extends Handler:
   override def handle(request: ServerRequest,
-                      response: ServerResponse): Unit = response.send(s"*** Now: ${Instant.now.toString}")
+                      response: ServerResponse): Unit =
+    response.send(s"*** Now: ${Instant.now.toString}")
