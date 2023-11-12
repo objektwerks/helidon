f082a9362e3368f668e8cd5f5bde74f9838d6c24
objektwerks <objektwerks@runbox.com>
2023-11-12 06:41:46 -0500

    edited conf

--- src/main/scala/objektwerks/Conf.scala
+++ src/main/scala/objektwerks/Conf.scala
@@ -3,6 +3,6 @@ package objektwerks
 import io.helidon.config.Config
 
 object Conf:
-  val config = Config.create.get("server")
+  val config = Config.create.get("now")
   val url = config.get("url").asString.get
   val endpoint = config.get("endpoint").asString.get
