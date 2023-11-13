eef67e31d6b8c2394d128f824a59d75d634d61c6
objektwerks <objektwerks@runbox.com>
2023-11-13 08:38:36 -0500

    edited now server

--- src/main/scala/objektwerks/NowServer.scala
+++ src/main/scala/objektwerks/NowServer.scala
@@ -6,14 +6,14 @@ import io.helidon.webserver.http.{Handler, HttpRouting}
 @main def runNowServer: Unit =
   val handler = NowHandler()
 
-  val routing = HttpRouting
+  val builder = HttpRouting
     .builder
     .get(Conf.endpoint, handler)
 
   WebServer
     .builder
     .config(Conf.config)
-    .routing(routing)
+    .routing(builder)
     .build
     .start
 
