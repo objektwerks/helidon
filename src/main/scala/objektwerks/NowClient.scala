c6fd90aaa93e660370b07766f21d64e743883a96
objektwerks <objektwerks@runbox.com>
2023-11-13 08:24:15 -0500

    simplified now client

--- src/main/scala/objektwerks/NowClient.scala
+++ src/main/scala/objektwerks/NowClient.scala
@@ -4,17 +4,11 @@ import io.helidon.webclient.api.WebClient
 
 object NowClient:
   def call(): String =
-    val client = WebClient
+    WebClient
       .builder
       .baseUri(Conf.url)
       .build
-
-    val now = client
       .get
       .request
       .entity
       .as(classOf[String])
-
-    println(s"*** Now Client get: ${Conf.url}")
-
-    now
