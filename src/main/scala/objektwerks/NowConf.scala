420f5c4ff22017fcde96bb805bbe7c95639613e8
objektwerks <objektwerks@runbox.com>
2023-11-17 07:40:50 -0500

    enhanced now conf

--- src/main/scala/objektwerks/NowConf.scala
+++ src/main/scala/objektwerks/NowConf.scala
@@ -4,5 +4,6 @@ import io.helidon.config.Config
 
 object NowConf:
   val config = Config.create.get("now")
+  val port = config.get("port").asInt.get
   val url = config.get("url").asString.get
   val endpoint = config.get("endpoint").asString.get
