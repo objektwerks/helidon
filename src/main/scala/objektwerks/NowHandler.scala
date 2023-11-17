f3c2a65013d83ce90b96d71e0eef68e6e872b3d8
objektwerks <objektwerks@runbox.com>
2023-11-17 07:30:47 -0500

    added endpoint to now handler

--- src/main/scala/objektwerks/NowHandler.scala
+++ src/main/scala/objektwerks/NowHandler.scala
@@ -5,6 +5,8 @@ import io.helidon.webserver.http.{Handler, ServerRequest, ServerResponse}
 import java.time.Instant
 
 final class NowHandler extends Handler:
+  val endpoint = Conf.endpoint
+
   override def handle(request: ServerRequest,
                       response: ServerResponse): Unit =
     response.send(s"*** Now: ${Instant.now.toString}")
