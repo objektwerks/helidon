857478ea424b8565e4a31efd42cbf10444665657
objektwerks <objektwerks@runbox.com>
2023-11-18 08:06:51 -0500

    edited event

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -3,7 +3,7 @@ package objektwerks
 import com.github.plokhotnyuk.jsoniter_scala.core.*
 import com.github.plokhotnyuk.jsoniter_scala.macros.*
 
-final case class Event(name: String)
+final case class Event(name: String = "event")
 
 object Event:
   given JsonValueCodec[Event] = JsonCodecMaker.make[Event]
