2d5075fae105c209f0771f822d4c1bb9b80b28e1
objektwerks <objektwerks@runbox.com>
2023-11-18 08:03:00 -0500

    added json support to event

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -1,3 +1,9 @@
 package objektwerks
 
+import com.github.plokhotnyuk.jsoniter_scala.core.*
+import com.github.plokhotnyuk.jsoniter_scala.macros.*
+
 final case class Event(name: String)
+
+object Event:
+  given JsonValueCodec[Event] = JsonCodecMaker.make[Event]
