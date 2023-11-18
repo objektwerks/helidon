b6f87a847c05ba343f12381915dc53b3c69df7e4
objektwerks <objektwerks@runbox.com>
2023-11-18 08:02:18 -0500

    added json support to command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -1,3 +1,9 @@
 package objektwerks
 
+import com.github.plokhotnyuk.jsoniter_scala.core.*
+import com.github.plokhotnyuk.jsoniter_scala.macros.*
+
 final case class Command(name: String)
+
+object Command:
+  given JsonValueCodec[Command] = JsonCodecMaker.make[Command]
