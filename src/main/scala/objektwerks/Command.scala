25145991be518b67565b447213b26e0017afb7fb
objektwerks <objektwerks@runbox.com>
2023-11-18 08:06:34 -0500

    edited command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -3,7 +3,7 @@ package objektwerks
 import com.github.plokhotnyuk.jsoniter_scala.core.*
 import com.github.plokhotnyuk.jsoniter_scala.macros.*
 
-final case class Command(name: String)
+final case class Command(name: String = "command")
 
 object Command:
   given JsonValueCodec[Command] = JsonCodecMaker.make[Command]
