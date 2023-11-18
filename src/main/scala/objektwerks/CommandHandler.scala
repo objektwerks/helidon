8df0367f9a9c27e8ee8058fcf20b99289e44050c
objektwerks <objektwerks@runbox.com>
2023-11-18 07:57:37 -0500

    added command handler

---
+++ src/main/scala/objektwerks/CommandHandler.scala
@@ -0,0 +1,10 @@
+package objektwerks
+
+import io.helidon.webserver.http.{Handler, ServerRequest, ServerResponse}
+
+import java.time.Instant
+
+final class CommandHandler() extends Handler:
+  override def handle(request: ServerRequest,
+                      response: ServerResponse): Unit =
+    response.send(s"*** Now: ${Instant.now.toString}")
