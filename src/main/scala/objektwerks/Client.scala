005a43bbd2e1e64b7be3c0c428aa8f0c5cf06d8f
objektwerks <objektwerks@runbox.com>
2023-11-18 08:18:00 -0500

    added post to client

--- src/main/scala/objektwerks/Client.scala
+++ src/main/scala/objektwerks/Client.scala
@@ -1,5 +1,7 @@
 package objektwerks
 
+import com.github.plokhotnyuk.jsoniter_scala.core.*
+
 import io.helidon.webclient.api.WebClient
 
 object Client:
@@ -12,3 +14,17 @@ object Client:
       .request
       .entity
       .as(classOf[String])
+
+  def post(command: Command, url: String): Event =
+    val commandJson = writeToString[Command](command)
+
+    val eventJson = WebClient
+      .builder
+      .baseUri(url)
+      .build
+      .post(commandJson)
+      .request
+      .entity
+      .as(classOf[String])
+    
+    readFromString[Event](eventJson)
