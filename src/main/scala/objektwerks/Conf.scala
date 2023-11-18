6ff4d5d45f4358f87cf1f3670a283148911f7d9c
objektwerks <objektwerks@runbox.com>
2023-11-18 07:53:23 -0500

    added command config to conf

--- src/main/scala/objektwerks/Conf.scala
+++ src/main/scala/objektwerks/Conf.scala
@@ -9,3 +9,6 @@ object Conf:
 
   val nowEndpoint = config.get("now").get("endpoint").asString.get
   val nowUrl = config.get("now").get("url").asString.get
+
+  val commandEndpoint = config.get("command").get("endpoint").asString.get
+  val commandUrl = config.get("command").get("url").asString.get
