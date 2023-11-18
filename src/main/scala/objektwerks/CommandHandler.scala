6fdae4ad10292ff95080300a9880a7ccfcd079b6
objektwerks <objektwerks@runbox.com>
2023-11-18 08:09:53 -0500

    enhanced command handler

--- src/main/scala/objektwerks/CommandHandler.scala
+++ src/main/scala/objektwerks/CommandHandler.scala
@@ -7,6 +7,11 @@ import io.helidon.webserver.http.{Handler, ServerRequest, ServerResponse}
 final class CommandHandler() extends Handler:
   override def handle(request: ServerRequest,
                       response: ServerResponse): Unit =
-    val json = request.content.as(classOf[String])
-    val command = readFromString[Command](json)
+    val commandJson = request.content.as(classOf[String])
+    val command = readFromString[Command](commandJson)
     println(s"*** Command: $command")
+
+    val event = Event()
+    val eventJson = writeToString[Event](event)
+    println(s"*** Event: $eventJson")
+    response.send(eventJson)
