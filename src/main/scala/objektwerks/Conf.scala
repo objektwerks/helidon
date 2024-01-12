8f7fe8b60ab5019974139a8a41753472823525ce
objektwerks <objektwerks@runbox.com>
2024-01-12 06:21:40 -0500

    enhanced conf

--- src/main/scala/objektwerks/Conf.scala
+++ src/main/scala/objektwerks/Conf.scala
@@ -4,6 +4,7 @@ import io.helidon.config.Config
 
 object Conf:
   val config = Config.create
+  Config.global(config)
 
   val port = config.get("server").get("port").asInt.get
   val url = config.get("server").get("url").asString.get
@@ -13,3 +14,5 @@ object Conf:
   val commandEndpoint = config.get("command").get("endpoint").asString.get
 
   val metrics = config.get("server.features.observe")
+
+  val openapi = config.get("openapi")
