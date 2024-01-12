019e1f16442fafc62cd867aa35afe3c6fd67c06e
objektwerks <objektwerks@runbox.com>
2024-01-12 09:19:25 -0500

    enhanced server logging

--- src/main/scala/objektwerks/Server.scala
+++ src/main/scala/objektwerks/Server.scala
@@ -2,9 +2,14 @@ package objektwerks
 
 import io.helidon.logging.common.LogConfig
 import io.helidon.webserver.WebServer
+
+import org.slf4j.LoggerFactory
   
 @main def runServer: Unit =
   LogConfig.configureRuntime()
+  val logger = LoggerFactory.getLogger("app")
+  logger.info("Server initializing ...")
+
   
   WebServer
     .builder
@@ -17,6 +22,7 @@ import io.helidon.webserver.WebServer
     .build
     .start
 
+  logger.info("Server initialized.")
   println( Client.get(Conf.url, Conf.nowEndpoint) )
   println( Client.post(Command("run command"), Conf.url, Conf.commandEndpoint) )
 
