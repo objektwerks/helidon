631986b7b52501b63f24bc8e8fea668a581a2408
objektwerks <objektwerks@runbox.com>
2024-01-12 04:58:35 -0500

    added cors to router

--- src/main/scala/objektwerks/Router.scala
+++ src/main/scala/objektwerks/Router.scala
@@ -1,10 +1,23 @@
 package objektwerks
 
+import io.helidon.cors.CrossOriginConfig
+import io.helidon.webserver.cors.CorsSupport
 import io.helidon.webserver.http.HttpRouting
 import io.helidon.webserver.http.HttpRouting.Builder
 
 object Router:
   def build(): Builder = HttpRouting
     .builder
-    .get(Conf.nowEndpoint, NowHandler())
-    .post(Conf.commandEndpoint, CommandHandler())
+    .get(Conf.nowEndpoint, cors(), NowHandler())
+    .post(Conf.commandEndpoint, cors(), CommandHandler())
+
+  private def cors(): CorsSupport = CorsSupport.builder()  
+    .addCrossOrigin(
+      CrossOriginConfig
+        .builder() 
+        .allowOrigins("http://localhost")
+        .allowMethods("GET", "POST") 
+        .build()
+    ) 
+    .addCrossOrigin(CrossOriginConfig.create())
+    .build()
